a6e9b2ea59b1a2c78ba260c009e8a08f4791946f
yedai <yedai@iflytek.com>
2023-04-13 21:01:48 +0800

    add website title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="cupcake">
-      <Head />
+      <Head title='ChatGIF' />
       <body>
         <Main />
         <NextScript />
