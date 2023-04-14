d1111ae4dcf60fa2cdf2ce6ac3273208ddc60f07
yedai <yedai@iflytek.com>
2023-04-14 11:52:00 +0800

    website title set ChatGIF

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="cupcake">
-      <Head title='ChatGIF' />
+      <Head>
+        <title>ChatGIF</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
