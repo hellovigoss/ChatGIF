f588a3aa2c5d3d74060c1abd4dcabe587e01ab9a
yedai <yedai@iflytek.com>
2023-04-15 10:52:50 +0800

    import optimize

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { useEffect } from 'react'
 import { useState } from "react";
 import ReactMarkdown from 'react-markdown';
 import GithubConor from "../components/githubConor";
